9ff43dec7c248a66b177c281ebe1e5a454123dad
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2023-11-20 22:19:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: { origin: "*", methods: ["GET", "POST"] },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Server is running.");
+});
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded");
+    });
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal);
+    });
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
